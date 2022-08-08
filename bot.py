a5c9885c410836f5d1e7d3a8b82abab42c1c8370
jkdevil27 <91070111+jkdevil27@users.noreply.github.com>
2022-08-08 12:08:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ class Bot:
 
 TOKEN = os.environ.get("TOKEN", "")
 
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", 123456))
 
 API_HASH = os.environ.get("API_HASH", "")
 
